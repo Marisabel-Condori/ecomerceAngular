af1f31d7cae5d11e8f7c8360a46c4b78947f786b
mari <mari.17ymy@gmail.com>
2023-07-07 19:36:52 -0400

    typescript

---
+++ src/app/recap.ts
@@ -0,0 +1,13 @@
+const username = 'Mari'
+
+const sum = (a:number, b:number) =>{
+  return a+b
+}
+sum(1,2)
+
+class Person{
+
+  constructor(public age:number, public lastname:string){ }
+}
+
+const nico = new Person(15, 'Molina')
