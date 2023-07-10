6bf559e8e33619211cd746c6032009174f763cdd
mari <mari.17ymy@gmail.com>
2023-07-09 23:57:34 -0400

    property biding - Los corchetes [ ] (square brakets) hacen que Angular evalúe el lado derecho de la asignación como una expresión dinámica. Sin los corchetes, Angular trata el lado derecho como un literal de cadena y establece la propiedad en ese valor estático.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   name = 'Mari';
   age = 20;
   imagen = 'https://assets.dev-filo.dift.io/img/2021/08/05/rocket_portada_sq.jpg'
+  btnDisabled = true;
+
+  person={
+    name: 'Mari',
+    age:18,
+    avatar: 'https://assets.dev-filo.dift.io/img/2021/08/05/rocket_portada_sq.jpg'
+
+  }
 }
